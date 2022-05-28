8b21c368a7a58b455373ea62b2199ba51d8f4b0d
Augustin <catalin.rotar@student.upt.ro>
2022-05-28 10:33:12 +0300

    initial commit

---
+++ src/pacman/Logic.java
@@ -0,0 +1,13 @@
+package pacman;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Logic extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
