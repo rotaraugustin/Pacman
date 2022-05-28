89a7902dc837eb2d610b67e3c57086240516ee1c
Augustin <catalin.rotar@student.upt.ro>
2022-05-28 12:57:31 +0300

    keyboard+images

--- src/pacman/Logic.java
+++ src/pacman/Logic.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Logic extends JPanel implements ActionListener {
 
@@ -60,12 +61,55 @@ public class Logic extends JPanel implements ActionListener {
     }
 
     private void initVariables() {
+        screenData=new short[nBlocks*nBlocks];
+        dimension = new Dimension(400, 400);
+        ghostX = new int[maxGhosts];
+        ghostDx = new int[maxGhosts];
+        ghostY = new int[maxGhosts];
+        ghostDy = new int[maxGhosts];
+        ghostSpeed = new int[maxGhosts];
+        dx = new int[4];
+        dy = new int[4];
+        timer = new Timer(40, this);//speed of the game
+        timer.start();
     }
 
     private void loadImages() {
+        down = new ImageIcon("src/pacman/images/down.gif").getImage();
+        up = new ImageIcon("/src/pacman/images/up.gif").getImage();
+        left = new ImageIcon("/src/pacman/images/left.gif").getImage();
+        right = new ImageIcon("/src/pacman/images/right.gif").getImage();
+        ghost = new ImageIcon("/src/pacman/images/ghost.gif").getImage();
+        heart = new ImageIcon("/src/pacman/images/heart.png").getImage();
     }
     class TAdapter extends KeyAdapter {
+        public void keyPressed(KeyEvent e) {
 
+            int key = e.getKeyCode();
+
+            if (inGame) {
+                if (key == KeyEvent.VK_LEFT) {
+                    reqDx = -1;
+                    reqDy = 0;
+                } else if (key == KeyEvent.VK_RIGHT) {
+                    reqDx = 1;
+                    reqDy = 0;
+                } else if (key == KeyEvent.VK_UP) {
+                    reqDx = 0;
+                    reqDy = -1;
+                } else if (key == KeyEvent.VK_DOWN) {
+                    reqDx = 0;
+                    reqDy = 1;
+                } else if (key == KeyEvent.VK_ESCAPE && timer.isRunning()) {
+                    inGame = false;
+                }
+            } else {
+                if (key == KeyEvent.VK_SPACE) {
+                    inGame = true;
+                    initGame();
+                }
+            }
+        }
     }
 
     @Override
